1f5d19aa2c305ac3b38594af61bba7ad425d3815
Andres Sainz de Aja <andres.sainz@globant.com>
2014-01-22 20:44:03 -0300

    Update songs lis page:
    * Enable field validation
    * Enable/disable buttons according fields state
    * Add clear all button
    * Add song to songs list
    * Add input search
    * Order songs by title

--- app/scripts/controllers/songsList.js
+++ app/scripts/controllers/songsList.js
@@ -2,9 +2,56 @@
 
 angular.module('devApp')
   .controller('SongsListCtrl', function ($scope, $filter, $routeParams, AlbumsSrv) {
+    var isInvalidInput = function(field){
+        return field.$dirty && field.$invalid;
+    };
+
+    var isValidInput = function(field){
+        return field.$dirty && field.$valid;
+    };
+
     AlbumsSrv.get()
       .success(function(response){
         $scope.album = $filter('filter')(response.albums, $routeParams.album)[0];
       })
       .error(function(){});
+
+    $scope.newSong = {
+        title: '',
+        leadVocals: '',
+        length: ''
+    };
+
+    $scope.orderKey = 'title';
+
+    $scope.addSongToList = function(form){
+        $scope.album.songs.push({
+            'title': $scope.newSong.title,
+            'id': $scope.newSong.title,
+            'leadVocals': [ $scope.newSong.leadVocals ],
+            'length': $scope.newSong.length,
+            'moreinfo': true
+        });
+
+        $scope.clearForm(form);
+    };
+
+    $scope.getValidationClasses = function(field){
+        return {
+            'has-error': isInvalidInput(field),
+            'has-success': isValidInput(field)
+        };
+    };
+
+    $scope.clearForm = function(form){
+        form.$setPristine();
+        $scope.newSong = {
+            title: '',
+            leadVocals: '',
+            length: ''
+        };
+    };
   });
+
+
+  
